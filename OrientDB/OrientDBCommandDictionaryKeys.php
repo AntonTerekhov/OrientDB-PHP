c63d45bdcf7c8474e96856fd1bfdd7a8977d86b2
Anton Terekhov <anton.a.terekhov@gmail.com>
2011-02-17 09:06:23 +0300

    Tests for dictionaryKeys #0

--- OrientDB/OrientDBCommandDictionaryKeys.php
+++ OrientDB/OrientDBCommandDictionaryKeys.php
@@ -2,24 +2,24 @@
 
 class OrientDBCommandDictionaryKeys extends OrientDBCommandAbstract
 {
-	public function __construct($parent)
-	{
-		parent::__construct($parent);
-		$this->type = OrientDBCommandAbstract::DICTIONARY_KEYS;
-	}
+    public function __construct($parent)
+    {
+        parent::__construct($parent);
+        $this->type = OrientDBCommandAbstract::DICTIONARY_KEYS;
+    }
 
-	public function prepare()
-	{
-		parent::prepare();
-	}
+    public function prepare()
+    {
+        parent::prepare();
+    }
 
-	protected function parse()
-	{
+    protected function parse()
+    {
         $keysCount = $this->readInt();
         $keys = array();
         for ($i = 0; $i < $keysCount; $i++) {
-        	$keys[] = $this->readString();
+            $keys[] = $this->readString();
         }
         return $keys;
-	}
+    }
 }
