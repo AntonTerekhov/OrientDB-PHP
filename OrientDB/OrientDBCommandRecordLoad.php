cc07213f2ebfa46b6285542c79b3dd50b6383d52
Anton Terekhov <anton.a.terekhov@gmail.com>
2011-02-17 16:01:13 +0300

    Added tests for recordLoad() #0

--- OrientDB/OrientDBCommandRecordLoad.php
+++ OrientDB/OrientDBCommandRecordLoad.php
@@ -2,49 +2,56 @@
 
 class OrientDBCommandRecordLoad extends OrientDBCommandAbstract
 {
-	protected $clusterId;
+    protected $clusterID;
 
-	protected $recordId;
+    protected $recordPos;
 
-	public function __construct($parent)
-	{
-		parent::__construct($parent);
-		$this->type = OrientDBCommandAbstract::RECORD_LOAD;
-	}
+    public function __construct($parent)
+    {
+        parent::__construct($parent);
+        $this->type = OrientDBCommandAbstract::RECORD_LOAD;
+    }
 
-	public function prepare()
-	{
-		parent::prepare();
-		$record_ids = explode(':', $this->attribs[0]);
-		if (count($record_ids) != 2) {
-			throw new OrientDBException('Wrong format for record ID');
-		}
-		$this->clusterId = (int) $record_ids[0];
-		$this->recordId = (int) $record_ids[1];
-		// Add ClusterId
-        $this->addShort($this->clusterId);
+    public function prepare()
+    {
+        parent::prepare();
+        if (count($this->attribs) != 2) {
+            throw new OrientDBWrongParamsException('This command requires record ID and, optionally, fetch plan');
+        }
+        $arr = explode(':', $this->attribs[0]);
+        if (count($arr) != 2) {
+            throw new OrientDBWrongParamsException('Wrong format for record ID');
+        }
+        $this->clusterID = (int) $arr[0];
+        $this->recordPos = (int) $arr[1];
+
+        if ($this->clusterID === 0 || $this->recordPos === 0) {
+            throw new OrientDBWrongParamsException('Wrong format for record ID');
+        }
+        // Add ClusterId
+        $this->addShort($this->clusterID);
         // Add RecordId
-        $this->addLong($this->recordId);
+        $this->addLong($this->recordPos);
         // Fetchplan
         $this->addString($this->attribs[1]);
 
-	}
+    }
 
-	protected function parse()
-	{
+    protected function parse()
+    {
         $status = $this->readByte();
         if ($status != chr(0)) {
-        	$this->debugCommand('record_content');
-        	$record_content = $this->readBytes();
-        	$this->debugCommand('record_version');
-        	$record_version = $this->readInt();
-        	$this->debugCommand('record_type');
-        	$record_type = $this->readByte();
+            $this->debugCommand('record_content');
+            $record_content = $this->readBytes();
+            $this->debugCommand('record_version');
+            $record_version = $this->readInt();
+            $this->debugCommand('record_type');
+            $record_type = $this->readByte();
             $this->debugCommand('status');
             $status = $this->readByte();
             while ($status != chr(0)) {
-            	$this->debugCommand('record_content');
-            	$record = $this->readRecord();
+                $this->debugCommand('record_content');
+                $record = $this->readRecord();
                 $this->debugCommand('status');
                 $status = $this->readByte();
                 // @TODO Deal with caching entries
@@ -53,17 +60,15 @@ class OrientDBCommandRecordLoad extends OrientDBCommandAbstract
             $record = new OrientDBRecord();
             // @TODO fix classId
             $record->classId = null;
-	        $record->type = $record_type;
-	        $record->clusterId = $this->clusterId;
-	        $record->recordId = $this->recordId;
-	        $record->version = $record_version;
-	        $record->content = $record_content;
-	        $record->parse();
+            $record->type = $record_type;
+            $record->clusterId = $this->clusterID;
+            $record->recordPos = $this->recordPos;
+            $record->version = $record_version;
+            $record->content = $record_content;
+            $record->parse();
 
-	        return $record;
+            return $record;
         }
         return false;
-
-	}
-
+    }
 }
---
+++ Tests/OrientDBCommandRecordLoadTest.php
@@ -0,0 +1,100 @@
+<?php
+
+require_once 'OrientDB/OrientDB.php';
+
+class OrientDBRecordLoadTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Correct password for root can be found at
+     * config/orientdb-server-config.xml in your OrientDB installation
+     */
+    protected $root_password = '60F3D52B4374C22B19F2EA5AD2812A45FB1C34985C2532D60E267AADB9E3E130';
+
+    protected $db;
+
+    protected $clusterId = 1;
+
+    protected $recordContent = 'testrecord:0';
+
+    protected function setUp() {
+        $this->db = new OrientDB('localhost', 2424);
+    }
+
+    protected function tearDown() {
+        $this->db = null;
+    }
+
+    public function testRecordLoadOnNotConnectedDB() {
+        $this->setExpectedException('OrientDBWrongCommandException');
+        $list = $this->db->recordLoad();
+    }
+
+    public function testRecordLoadOnConnectedDB() {
+        $this->db->connect('root', $this->root_password);
+        $this->setExpectedException('OrientDBWrongCommandException');
+        $list = $this->db->recordLoad();
+    }
+
+    public function testRecordLoadOnNotOpenDB() {
+        $this->setExpectedException('OrientDBWrongCommandException');
+        $list = $this->db->recordLoad();
+    }
+
+    public function testRecordLoadOnOpenDB() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        $recordPos = $this->db->recordCreate($this->clusterId, $this->recordContent);
+        $record = $this->db->recordLoad($this->clusterId . ':' . $recordPos, '');
+        $this->assertInstanceOf('OrientDBRecord', $record);
+        $this->assertAttributeEquals($this->recordContent, 'content', $record);
+        $result = $this->db->recordDelete($this->clusterId . ':' . $recordPos);
+        $this->assertTrue($result);
+    }
+
+    public function testRecordLoadWithWrongOptionCount() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        $this->setExpectedException('OrientDBWrongParamsException');
+        $record = $this->db->recordLoad();
+    }
+
+    public function testRecordLoadWithWrongRecordIDOne() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        $this->setExpectedException('OrientDBWrongParamsException');
+        $record = $this->db->recordLoad('INVALID', '');
+    }
+
+    public function testRecordLoadWithWrongRecordIDTwo() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        $this->setExpectedException('OrientDBWrongParamsException');
+        $record = $this->db->recordLoad('INVALID:', '');
+    }
+
+    public function testRecordLoadWithWrongRecordIDThree() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        $this->setExpectedException('OrientDBWrongParamsException');
+        $record = $this->db->recordLoad(':INVALID', '');
+    }
+
+    public function testRecordLoadWithDeletedRecordId() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        $recordPos = $this->db->recordCreate($this->clusterId, $this->recordContent);
+        $this->assertInternalType('integer', $recordPos);
+        $result = $this->db->recordDelete($this->clusterId . ':' . $recordPos);
+        $this->assertTrue($result);
+        $record = $this->db->recordLoad($this->clusterId . ':' . $recordPos, '');
+        $this->assertFalse($record);
+    }
+
+    public function testRecordLoadWithOutOfBoundsRecordId() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        $this->setExpectedException('OrientDBException');
+        $record = $this->db->recordLoad($this->clusterId . ':' . 1000000, '');
+    }
+
+    public function testRecordLoadWithFetchPlan() {
+        $this->db->DBOpen('demo', 'writer', 'writer');
+        // Load record City:1
+        $record = $this->db->recordLoad(12 . ':' . 1, '*:-1');
+        $this->assertInstanceOf('OrientDBRecord', $record);
+    }
+}
